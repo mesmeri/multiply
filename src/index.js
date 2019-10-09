8a113a6f095e6b813b5d6288667bcb3d3b1104d4
OksanaShuptar <angels_ua@ukr.net>
2019-10-10 00:44:04 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function multiply(first, second) {
-  // your solution
+
+  first = first.split('').map(digit => parseInt(digit, 10));
+  second = second.split('').map(digit => parseInt(digit, 10));
+
+  let firstNum = first.length;
+  let secondNum = second.length;
+  
+  let result = new Array((firstNum + secondNum));
+
+  result.fill(0);
+  
+  for (let j = secondNum - 1; j >= 0; j--) {
+  
+      for (let i = firstNum - 1; i >= 0; i--) {
+          let c = second[j] * first[i];
+          let temp = result[i + j + 1];
+  
+          result[i + j + 1] = (temp + c) % 10;
+          result [i + j] = result[i + j] + ((temp + c) - result[i + j + 1]) / 10;
+      }
+  
+  }
+  
+  while (result[0] === 0) {
+    result.shift();
+  }
+
+  return result.join('');
+
 }
